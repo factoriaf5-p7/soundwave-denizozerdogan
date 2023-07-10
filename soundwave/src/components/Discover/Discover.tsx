951ee905cb3b2ed24507cc6d774e1060e2ba86af
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-07-10 22:33:51 +0200

    discover laptop done, all sections completed

--- soundwave/src/components/Discover/Discover.tsx
+++ soundwave/src/components/Discover/Discover.tsx
@@ -9,20 +9,24 @@ import { AlbumCover } from "../../Atoms/AlbumCover/AlbumCover";
 export const Discover = () => {
   return (
     <div id="discover-section">
-      <div className="title">
-        <Title text="Discover new music" />
-      </div>
-      <div className="icons">
-        <ChartIcon />
-        <AlbumIcon />
-        <MoreIcon />
-      </div>
-      <div className="text">
-        <SmallTextField value="By joining you can benefit by listening to the latest albums released." />
-      </div>
-      <div className="album-covers">
-        <AlbumCover />
-       </div>
+     <div className="title-icons-text">
+    <div className="title">
+      <Title text="Discover new music" />
+    </div>
+    <div className="icons">
+      <ChartIcon />
+      <AlbumIcon />
+      <MoreIcon />
+    </div>
+    <div className="text">
+      <SmallTextField value="By joining you can benefit by listening to the latest albums released." />
+    </div>
+  </div>
+  <div className="album-covers-section">
+    <div className="album-covers">
+      <AlbumCover />
+    </div>
+  </div>
     </div>
   );
 }
